384fefcda12797e840df5c3df9894d5b8aa26757
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-05-16 14:03:40 -0400

    setup jest

---
+++ src/components/Button/__tests__/Button.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import Button from '..'
+
+describe('Test', () => {
+  it('Should be able to test a simple calculation', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
